fa35cbfdd2d0fe15e22ef3dc1b206c7c4dc3d44f
Hieu Pham <hieu02617@gmail.com>
2025-01-06 23:52:24 +0700

    #21

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,16 +1,26 @@
-let getHomePage = (req, res) =>  {
-    return res.render('homePage.ejs');
-}
+import db from "../models/index";
+
+let getHomePage = async (req, res) => {
+  try {
+    let data = await db.User.findAll();
+
+    return res.render("homePage.ejs", {
+      data: JSON.stringify(data),
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 let getAboutPage = (req, res) => {
-    return res.render('test/about.ejs');
-}
+  return res.render("test/about.ejs");
+};
 
 // object: {
 //     key: ''
 //     value: ''
 // }
 module.exports = {
-    getHomePage: getHomePage,
-    getAboutPage: getAboutPage
+  getHomePage: getHomePage,
+  getAboutPage: getAboutPage,
 };
