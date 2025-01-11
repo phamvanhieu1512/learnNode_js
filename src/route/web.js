8d3da629a4e101da02b21038c34c46e6700a388b
Hieu Pham <hieu02617@gmail.com>
2025-01-11 17:44:56 +0700

    #28

--- src/route/web.js
+++ src/route/web.js
@@ -12,6 +12,9 @@ let initWebRoutes = (app) => {
 
   router.get("/edit-crud", homeController.getEditCRUD);
   router.post("/put-crud", homeController.putCRUD);
+
+  router.get("/delete-crud", homeController.deleteCRUD);
+
   // rest API
 
   return app.use("/", router);
